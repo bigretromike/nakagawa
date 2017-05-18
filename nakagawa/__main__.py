d2d65564126149189b9a08ac7665d55d50ae5303
bigretromike <bigretromike@monogatari.pl>
2017-05-18 11:13:53 +0200

    Proper saving limit/404 status;

--- nakagawa/__main__.py
+++ nakagawa/__main__.py
@@ -4,12 +4,15 @@
 import nakagawa
 import time
 
-print("Nakagawa init...")
 n = nakagawa.Nakagawa()
+n.logger.info("Nakagawa: starting")
 
 while True:
-    print("{}".format(str(time.time())))
+    n.logger.info(time.strftime("%d-%m-%Y %H:%M:%S"))
+    n.logger.debug("wallabag: checking")
     n.check_wallabag()
+    n.logger.debug("4chan: checking")
     n.check_4chan()
+    n.logger.debug("4chan: watching")
     n.watch_4chan()
     time.sleep(300)  # 5minute sleep
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='nakagawa',
-      version="0.1",
+      version="0.2",
       description='Nakagawa - central river',
       author='bigretromike',
       url='https://github.com/bigretromike/nakagawa/',
@@ -14,7 +14,8 @@ setup(name='nakagawa',
       install_requires=[
           'sqlalchemy>=1.1.9',
           'wallabag_api>=1.1.0',
-          'BASC-py4chan>=0.6.3'
+          'BASC-py4chan>=0.6.3',
+          'logging>=0.4.9.6'
       ],
       license='MIT'
       )
